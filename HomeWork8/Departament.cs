05f22de12802f8647ae3b3c67b316806e1e7ccd8
Grigiriy <gkazunko@mail.ru>
2021-09-25 00:39:59 +0300

    Добавьте файлы проекта.

---
+++ HomeWork8/Departament.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork8
+{
+    class Departament
+    {
+
+        public Departament(string DepName, DateTime DateCreate, int NumberWorkers)
+        {
+            this.depName = DepName;
+            this.dateCreate = DateCreate;
+            this.numberWorkers = NumberWorkers;
+        }
+
+        /// <summary>
+        /// Наименование департамента
+        /// </summary>
+        public string depName { get; set; }
+        /// <summary>
+        /// Дата основания департамента
+        /// </summary>
+        public DateTime dateCreate { get; set; }
+        /// <summary>
+        /// Количество сотрудников в департаменте
+        /// </summary>
+        public int numberWorkers { get; set; }
+
+    }
+}
+
---
+++ HomeWork8/Worker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork8
+{
+    class Worker
+    {
+        public Worker(int TabNumber, string LastName, string FirstName, int Age, string Dep, int Salary)
+        {
+            this.tabNumber = TabNumber;
+            this.lastName = LastName;
+            this.firstName = FirstName;
+            this.age = Age;
+            this.dep = Dep;
+            this.salary = Salary;
+        }
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int tabNumber { get; set; }
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string lastName { get; set; }
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string firstName { get; set; }
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public int age { get; set; }
+        /// <summary>
+        /// Департамент
+        /// </summary>
+        public string dep { get; set; }
+        /// <summary>
+        /// Заработная плата
+        /// </summary>
+        public int salary { get; set; }
+        /// <summary>
+        /// Вывод данных на экран
+        /// </summary>
+        /// <returns></returns>
+    }
+}
